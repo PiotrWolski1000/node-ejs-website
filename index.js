e0ad7a8d4af809783279b68457a74c89750b7466
piotr <p.wolski1000@gmail.com>
2018-04-28 18:56:45 +0200

    changing css

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
-
+// const Typed = require('typed.js');
 const port  = 3000
 
 app.set('view engine', 'ejs')
 //static files
 app.set('views', './views')
 
-app.use(express.static("public"))
+app.use(express.static("assets"))
 app.use(express.static("js"))
 app.use(express.static("styles"))
 
 app.get( '/',  (req, res) => {
-    res.render('app')
+    res.render('app', {
+        title: "Piotr Wolski"
+    })
 })
 
 app.listen(port)
