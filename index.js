c5109c213ed6bf4508d43e11008fb97fa3b0e87c
piotr <p.wolski1000@gmail.com>
2018-05-06 21:49:56 +0200

    adding aobut section and changing some styles

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express');
-const app = express();
-// const Typed = require('typed.js');
-const port  = 3000
+const dotenv = require('dotenv')
+dotenv.config()
+const bodyParser = require('body-parser');
+const express = require('express')
+const sgMail = require('@sendgrid/mail')
 
-app.set('view engine', 'ejs')
+const app = express()
+const port  = 5000
+
+app.use(bodyParser.urlencoded({ extended: true }));
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
 //static files
+app.set('view engine', 'ejs')
 app.set('views', './views')
 
 app.use(express.static("assets"))
@@ -15,6 +22,27 @@ app.get( '/',  (req, res) => {
     res.render('app', {
         title: "Piotr Wolski"
     })
+    
 })
+
+
+app.post('/', (req, res) => {
+    const msg = {
+        to: process.env.TO,
+        from: process.env.FROM,
+        subject: req.body.mail_subject,
+        text: "Message from " + req.body.name + ": " + req.body.message + ", phone number: " + req.body.phone,
+        // html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+    
+    sgMail.send(msg);
+
+    res.render( 'app', { title: 'Piotr Wolski', message : "Message will be delivered as soon as possible, thank you :)" } );
+    
+    // console.log('Mail sended')
+    // res.send("elo xd")
+})
+
+
 
 app.listen(process.env.PORT || port)
